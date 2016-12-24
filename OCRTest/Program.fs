a23567ede4acc1565624050d4899baa882a46480
Kimserey Lam <kimserey.lam@groomgy.com>
2016-12-24 10:38:04 +0000

    convert pdf

--- OCRTest/Program.fs
+++ OCRTest/Program.fs
@@ -1,20 +1,35 @@
 ﻿module OCRTest.Root
 
-open ImageMagick
 open System
+open System.IO
+open ImageMagick
 open Tesseract
 
 [<EntryPoint>]
 let main argv = 
     use engine = new TesseractEngine("tessdata", "eng", EngineMode.Default)
-    use img = Pix.LoadFromFile "test/test_2.png"
+    use img = Pix.LoadFromFile "test/test.tif"
     use page = engine.Process img
 
     let text = page.GetText()
 
     printfn "%s %f" text (page.GetMeanConfidence())
+    
+    Directory.CreateDirectory("output") |> ignore
 
-    ImageMagick.PdfReadDefines
+    // Settings the density to 300 dpi will create an image with a better quality
+    let settings = new MagickReadSettings(Density = new Density(300.))
 
-    Console.ReadKey() |> ignore
+    use images = new MagickImageCollection()
+
+    // Add all the pages of the pdf file to the collection
+    images.Read("test/Snakeware.pdf", settings)
+
+    images 
+    |> Seq.toList
+    |> List.iteri (fun index image ->
+        sprintf "output/Snakeware_%i.png" index
+        |> image.Write
+    )
+    
     0 
